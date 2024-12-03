61b0f277b29bdc09be9d34ea26c8f0257e45e3be
Алексей <leha2017tv@gmail.com>
2024-12-03 08:47:47 +0500

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+        }
+      ],
+      // bodyAttrs: {
+      //   class: 'd-flex flex-colum h-100'
+      // },
+
+
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+          tagPosition: 'bodyClose',
+
+        }
+      ],
+    },
+
+  }
+})
